60b530949b4f36f8ba46fd8f3f5b316dc4df9b4c
Christina Ku Pei San <u2102789@siswa.um.edu.my>
2022-12-05 14:40:24 +0800

    Initial commit

---
+++ app/src/main/java/wia2007/example/jomchat/MainActivity.java
@@ -0,0 +1,14 @@
+package wia2007.example.jomchat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
