1177e7b196320354ff0ff47137b71439ce5f0bb2
LAPTOP-BAC20L4T\USER <sangkari276@gmail.com>
2022-12-10 22:49:11 +0800

    I added ProfileActivity

---
+++ app/src/main/java/wia2007/example/jomchat/ProfileActivity.java
@@ -0,0 +1,14 @@
+package wia2007.example.jomchat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
