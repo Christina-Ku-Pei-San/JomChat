62e4e75f37ffab6de6d5103d0c147ea0e093cc84
sssrnyx <serenaooi5908@gmail.com>
2023-01-02 14:59:30 +0800

    Dark Mode

--- app/src/main/java/wia2007/example/jomchat/SettingActivity.java
+++ app/src/main/java/wia2007/example/jomchat/SettingActivity.java
@@ -1,8 +1,11 @@
 package wia2007.example.jomchat;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +16,9 @@ public class SettingActivity extends AppCompatActivity {
     ImageView ivHome, ivMessenger, ivNotification;
     Button btnViewProfile, btnChangePassword, btnLogout;
     Switch swDarkMode;
+    boolean nightMODE;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,8 +61,33 @@ public class SettingActivity extends AppCompatActivity {
             }
         });
 
+        getSupportActionBar().hide();
+
         swDarkMode = findViewById(R.id.SwDarkMode);
 
+        sharedPreferences = getSharedPreferences("MODE", Context.MODE_PRIVATE);
+        nightMODE = sharedPreferences.getBoolean("night", false);
+
+        if (nightMODE){
+            swDarkMode.setChecked(true);
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }
+
+        swDarkMode.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+              if (nightMODE){
+                  AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                  editor = sharedPreferences.edit();
+                  editor.putBoolean("night", false);
+              } else{
+                  AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                  editor = sharedPreferences.edit();
+                  editor.putBoolean("night", true);
+              }
+              editor.apply();
+          }
+        });
         btnChangePassword = findViewById(R.id.BtnChangePassword);
         btnChangePassword.setOnClickListener(new View.OnClickListener() {
             @Override
