a09d15cbfe6ccd05c13ac0cb5c4cdef4c9fb692c
LAPTOP-BAC20L4T\USER <sangkari276@gmail.com>
2023-01-07 02:44:02 +0800

    edited some change password activity but to get the exiting password from user im not sure how to edit. I added the shared preference import and codes from login but i couldnt change it.

--- app/src/main/java/wia2007/example/jomchat/ChangePasswordActivity.java
+++ app/src/main/java/wia2007/example/jomchat/ChangePasswordActivity.java
@@ -3,16 +3,53 @@ package wia2007.example.jomchat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
+
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.regex.Pattern;
+
 
 public class ChangePasswordActivity extends AppCompatActivity {
-    ImageView ivBack, ivHome, ivMessenger, ivNotification;
-    Button btnConfirm;
+    private static final Pattern PASSWORD_PATTERN =
+            Pattern.compile("^" +
+                    "(?=.*[0-9])" +         //at least 1 digit
+                    "(?=.*[a-z])" +         //at least 1 lower case letter
+                    "(?=.*[A-Z])" +         //at least 1 upper case letter
+                    //"(?=.*[a-zA-Z])" +      //any letter
+                    "(?=.*[@#_$%^&+=])" +    //at least 1 special character
+                    "(?=\\S+$)" +           //no white spaces
+                    ".{8,}" +               //at least 8 characters
+                    "$");
 
+    private TextInputLayout textInputCurrentPassword;
+    private TextInputLayout textInputNewPassword;
+    private TextInputLayout textInputConfirmNewPassword;
+
+    private ImageView ivBack, ivHome, ivMessenger, ivNotification;
+    private Button btnConfirm;
+
+    private String usernameInput, current_passwordInput, new_passwordInput, confirm_new_passwordInput;
     String username;
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference databaseReference = database.getReferenceFromUrl("https://jomchat-9f535-default-rtdb.asia-southeast1.firebasedatabase.app/");
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +57,9 @@ public class ChangePasswordActivity extends AppCompatActivity {
         setContentView(R.layout.activity_change_password);
 
         username = getIntent().getStringExtra("username");
+        textInputCurrentPassword = findViewById(R.id.current_password);
+        textInputNewPassword = findViewById(R.id.new_password);
+        textInputConfirmNewPassword = findViewById(R.id.new_password2);
 
         ivBack = findViewById(R.id.IVBack);
         ivBack.setOnClickListener(new View.OnClickListener() {
@@ -68,7 +108,88 @@ public class ChangePasswordActivity extends AppCompatActivity {
                 Intent startintent = new Intent(ChangePasswordActivity.this, SettingActivity.class);
                 startintent.putExtra("username", username);
                 startActivity(startintent);
+
+                current_passwordInput = textInputCurrentPassword.getEditText().getText().toString().trim();
+                new_passwordInput = textInputNewPassword.getEditText().getText().toString().trim();
+                confirm_new_passwordInput = textInputConfirmNewPassword.getEditText().getText().toString().trim();
+                changePasswordInput(view);
             }
         });
+    }
+
+    private boolean validateCurrentPassword() {
+        if (current_passwordInput.isEmpty()) {
+        textInputNewPassword.setError("Field can't be empty");
+        return false;
+        }  else if (!current_passwordInput.equals(current_passwordInput)) {
+            textInputConfirmNewPassword.setError("Current Password does not match");
+            return false;
+        }
+        else {
+        textInputCurrentPassword.setError(null);
+        return true;
+    }
+}
+
+    private boolean validateNewPassword() {
+        if (new_passwordInput.isEmpty()) {
+            textInputNewPassword.setError("Field can't be empty");
+            return false;
+        }  else if (!PASSWORD_PATTERN.matcher(new_passwordInput).matches()) {
+            textInputNewPassword.setError("New Password too weak");
+            return false;
+        }
+        else {
+            textInputNewPassword.setError(null);
+            return true;
+        }
+    }
+
+    private boolean validateConfirmNewPassword() {
+        if (confirm_new_passwordInput.isEmpty()) {
+            textInputConfirmNewPassword.setError("Field can't be empty");
+            return false;
+        }  else if (!new_passwordInput.equals(confirm_new_passwordInput)) {
+            textInputConfirmNewPassword.setError("Confirm New Password does not match");
+            return false;
+        }
+        else {
+            textInputConfirmNewPassword.setError(null);
+            return true;
+        }
+    }
+
+    private void changePasswordInput(View v) {
+        if (!validateCurrentPassword() | validateNewPassword() | validateConfirmNewPassword()) {
+            return;
+        }
+        else {
+            databaseReference.child("passwords").addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    // check if username is not registered before
+                    if (snapshot.hasChild(usernameInput)) {
+                        Toast.makeText(getApplicationContext(), "Username exists", Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        // sending data to firebase Realtime Database
+                        // we are using username as unique identity for every user
+                        // so all the other details of user comes under username
+                        databaseReference.child("passwords").child(usernameInput).child("newpassword").setValue(new_passwordInput);
+
+                        Toast.makeText(getApplicationContext(), "New Password Added Sucessfully", Toast.LENGTH_SHORT).show();
+
+                        Intent startintent = new Intent(ChangePasswordActivity.this, ProfileActivity.class);
+                        startActivity(startintent);
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+
+        }
     }
 }
